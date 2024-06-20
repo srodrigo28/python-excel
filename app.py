28884c3454cd520db60c9709a266ad0c94ee68e2
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-06-20 11:21:21 -0300

    abrindo-site

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import openpyxl
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
 
 planilha_clientes = openpyxl.load_workbook('dados_clientes.xlsx')
 pagina_clientes = planilha_clientes['Sheet1']
 
 for linha in pagina_clientes.iter_rows(min_row=2,values_only=True):
     nome, valor, cpf, vencimento = linha
-    print(nome)
-    print(cpf)
+    # print(nome)
+
+    driver = webdriver.Chrome()
+    driver.get('https://consultcpf-devaprender.netlify.app/')
+    sleep(5)
